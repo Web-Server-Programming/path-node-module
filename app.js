3071ea2617b66961c3f43eb43723ea99f0c77b0b
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-04 22:20:31 +0530

    Add. Path modules methods are console logged.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("This is app.js")
+const pathModule = require('path')
+
+console.log(pathModule.sep)
+
+const filePath = pathModule.join('/content/', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const baseName = pathModule.basename(filePath)
+console.log(baseName)
+
+const absolute = pathModule.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(absolute)
